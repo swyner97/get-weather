81717717f6477f23684145a6ebc01ba1a4fe404f
swyner97 <swyner97@gmail.com>
2022-08-27 20:30:07 -0700

    trouble appending

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,35 @@
-let city = document.querySelector('input');
+let city = document.querySelector('.input');
 let button = document.getElementById('search-button');
 // let cityOutput = documnet.getElementById('city');
-let daysOutput = document.getElementById('5-day')
-
-
-
-// let consoleStuff = () => {
-//     console.log(searchVal.value);
-// }
-
-// button.addEventListener('click', consoleStuff)
-
+let daysOutput = document.getElementById('5-day');
 const options = { method: 'GET' };
 
-let fetchFunc = () => {
-    fetch('http://api.openweathermap.org/data/2.5/weather?q=' + city.value + '&appid=06f216875a2540097378a73527e9598f', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
-}
-
-
 let fetchWeather = () => {
-    button.addEventListener('click', fetchFunc);
+    button.addEventListener('click', fetchFunc = () => {
+
+
+        fetch('http://api.openweathermap.org/data/2.5/weather?q=' + city.value + '&units=imperial&appid=856e91804556ec4ae091de73a2ba434f', options)
+            .then(response => response.json())
+            .then(response => {
+                let cityDiv = document.querySelector('#cityDiv')
+                let cityName = document.createElement('h1');
+                cityName.innerHTML = city.value;
+                let temp = document.createElement('p');
+                let wind = document.createElement('p')
+                let getTemp = response.main.temp;
+                let getWind = response.wind.speed;
+                
+                temp.innerHTML = getTemp + `°F`;
+                wind.innerHTML = getWind;
+
+                cityDiv.appendChild(cityName)
+            })
+            .catch(err => console.error(err));
+
+
+    }
+    );
 }
+
 
 fetchWeather();
